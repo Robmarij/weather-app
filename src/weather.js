2fe6efcd68471fde201a91b5ee75b7d243a1a673
Robmarij <76428456+Robmarij@users.noreply.github.com>
2021-02-07 16:36:42 +0200

    added a function to js

--- src/weather.js
+++ src/weather.js
@@ -75,16 +75,16 @@ function showLocation(event) {
 }
 function citateElement(){
   let citate = response.data.weather[0].description;
-  if (citate === "scattered clouds" | citate === "broken clouds");
+  if (citate === "scattered clouds" || citate === "broken clouds")
   {alert(`A gray day provides the best light - L. da Vinci`);
-} else if (citate === "shower rain" | citate === "rain" | citate === "thunderstorm");
+} else if (citate === "shower rain" || citate === "rain" || citate === "thunderstorm")
   {alert(`Without rain nothing grows`);
-} else if( citate === "clear sky" | citate ==="few clouds");
-{alert(`What sunshine is to flower, smiles are to humanity - J. Addison`);
-} else if (citate === "snow");
-{alert(`Kindness is like snow. I beautifies everything it covers - K. Gibran`);
-} else{
-  alert(`If you want to see the sunshine, you have to weather the storm - F. Lane`);
+} else if( citate === "clear sky" || citate ==="few clouds")
+  {alert(`What sunshine is to flower, smiles are to humanity - J. Addison`);
+} else if (citate === "snow")
+  {alert(`Kindness is like snow. I beautifies everything it covers - K. Gibran`);
+} else 
+ {alert(`If you want to see the sunshine, you have to weather the storm - F. Lane`);
 }
 }
 let weatherCitate = document.querySelector("p");
