a3bab4d7139ceec7b1b340f493a898d16eb13857
Robmarij <76428456+Robmarij@users.noreply.github.com>
2021-02-12 21:30:25 +0200

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -70,7 +70,7 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
 
-  apiUrl = `https//api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function enterCity(event) {
